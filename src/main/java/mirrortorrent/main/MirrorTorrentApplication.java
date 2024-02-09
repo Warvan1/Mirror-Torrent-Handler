28433ce573fdcfcf1085f970c648c84d5c786d53
warvan <collinsn@clarkson.edu>
2024-02-09 17:52:36 -0500

    Scrape With Depth added

--- src/main/java/mirrortorrent/main/MirrorTorrentApplication.java
+++ src/main/java/mirrortorrent/main/MirrorTorrentApplication.java
@@ -1,6 +1,7 @@
 package mirrortorrent.main;
 
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Vector;
 
 import mirrortorrent.torrents.*;
@@ -9,19 +10,18 @@ public class MirrorTorrentApplication {
     public static void main(String[] args){
 
         // String url = "https://linuxmint.com/torrents/";
-        String url = "https://torrents.artixlinux.org/torrents.php";
-        // String url = "https://download.documentfoundation.org/libreoffice/stable/"; // depth 5
+        // String url = "https://torrents.artixlinux.org/torrents.php";
+        String url = "https://download.documentfoundation.org/libreoffice/stable/"; // depth 5
 
-        try{
-            Vector<String> links = ScrapeTorrents.scrapeLinks(url, ".torrent");
-            
-            System.out.println(links);
+        String[] ignoreArray = {"apache", "mirrorbrain", "mailto", "?C=N;O=D", "?C=M;O=A", "?C=S;O=A", ".tar.gz", ".msi", ".asc", ".dmg", "exact: https://download.documentfoundation.org/libreoffice/", "exact: https://download.documentfoundation.org/libreoffice/stable/"};
+        // String[] ignoreArray = {"apache", "mirrorbrain", "mailto"};
+        Vector<String> ignoreList = new Vector<String>(Arrays.asList(ignoreArray));
 
-            ScrapeTorrents.downloadFileList(links, "torrent");
-        }
-        catch(IOException e){
-            e.printStackTrace();
-        }
+        HashSet<String> links = ScrapeTorrents.scrapeLinksDepth(url, ".torrent", ignoreList, 5 );
+        
+        System.out.println(links);
+
+        // ScrapeTorrents.downloadFileList(links, "torrent");
 
     }
 }
--- src/main/java/mirrortorrent/torrents/ScrapeTorrents.java
+++ src/main/java/mirrortorrent/torrents/ScrapeTorrents.java
@@ -5,39 +5,118 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashSet;
 import java.util.Vector;
 
-import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.ValidationException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.jsoup.UnsupportedMimeTypeException;
 
 public class ScrapeTorrents {
     
     private static final int BUFFER_SIZE = 4096;
 
-    //use jsoup to get the list of links from a given webpage
-    public static Vector<String> scrapeLinks(String url, String filterSuffix) throws IOException{
-        Vector<String> link_vec = new Vector<>();
-
-        //get the html from the page
-        Document doc = Jsoup.connect(url).get();
-        //get all the link elements on the page
-        Elements links = doc.getElementsByTag("a");
-        for (Element link : links){
-            //get the href url for each link and add it to the output vector if it ends in .torrent
-            String l = link.attr("abs:href");
-            if(filterSuffix.equals("") || (l.length() > filterSuffix.length() && l.substring(l.length() - filterSuffix.length()).equals(filterSuffix))){
-                link_vec.add(l);
-            }
+    public static HashSet<String> scrapeLinksDepth(String url, String filterSuffix, Vector<String> ignoreList, int depth){
+        //if depth is 1 return the output for the url from scrapeLinks
+        if(depth == 1){
+            return scrapeLinks(url, filterSuffix, ignoreList);
         }
 
-        return link_vec;
+        //if depth is NOT 1 we have to search all pages until given depth
+        HashSet<String> outputLinks = new HashSet<>();
+        HashSet<String> currientLinks = new HashSet<>();
+        currientLinks.add(url);
+
+        for(int i = depth; i >= 1; i--){
+            //base case where depth is 1
+            if(i == 1){
+                //retrieve all filtered links on each page and add them to outputLinks
+                for(String link : currientLinks){
+                    outputLinks.addAll(scrapeLinks(link, filterSuffix, ignoreList));
+                }
+            }
+            else{
+                //retrieve all links on each page and add them to new Links vector
+                HashSet<String> newLinks = new HashSet<>();
+                for(String link : currientLinks){
+                    newLinks.addAll(scrapeLinks(link, "", ignoreList));
+                }
+                //add to outputlinks any links from newLinks that end in filterSuffix
+                for(String l : newLinks){
+                    if(checkSuffix(l, filterSuffix)){
+                        outputLinks.add(l);
+                    }
+                }
+                //replace currientLinks with newLinks
+                currientLinks = newLinks;
+            }
+            System.out.println(currientLinks);
+            System.out.println(currientLinks.size());
+            System.out.println("--------------");
+            System.out.println(outputLinks);
+            System.out.println(outputLinks.size());
+            System.out.println("--------------");
+            System.out.println(i);
+            System.out.println("--------------");
+            System.out.println("--------------");
+
+        }
+
+        return outputLinks;
+    }
+
+    //use jsoup to get the list of links from a given webpage
+    public static HashSet<String> scrapeLinks(String url, String filterSuffix, Vector<String> ignoreList){
+        HashSet<String> link_set = new HashSet<>();
+
+        try{
+            //get the html from the page
+            Document doc = Jsoup.connect(url).get();
+            //get all the link elements on the page
+            Elements links = doc.getElementsByTag("a");
+            for (Element link : links){
+                //get the href url for each link and add it to the output vector if the link ends in "filterSuffix"
+                String l = link.attr("abs:href");
+                if(link.text().toLowerCase().equals("Parent Directory")) continue;
+                if(checkSuffix(l, filterSuffix) && !checkIgnoreList(l, filterSuffix, ignoreList)){
+                    link_set.add(l);
+                }
+            }
+        }
+        catch(IOException | ValidationException e){
+            // System.err.println(url);
+            // System.err.println(e);
+            // System.err.println("----------------------");
+            return new HashSet<>();
+        }
+
+        return link_set;
+    }
+
+    public static boolean checkSuffix(String s, String filterSuffix){
+        if(filterSuffix.equals("") || (s.length() > filterSuffix.length() && s.substring(s.length() - filterSuffix.length()).equals(filterSuffix))){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkIgnoreList(String s, String filterSuffix, Vector<String> ignoreList){
+        for(String l : ignoreList){
+            //exact case
+            if(l.startsWith("exact: ")){
+                if(s.equals(l.substring(7))){
+                    return true;
+                }
+            }
+            //normal contains case
+            else if(s.contains(l) && !s.contains(".torrent") && !s.contains(".mirrorlist")){
+                return true;
+            }
+        }
+        return false;
     }
 
     //download files from a vector of urls into a given file location
